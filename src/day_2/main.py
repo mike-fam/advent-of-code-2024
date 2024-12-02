598b4b16908fdc3eea942f2e3e937dd1e7d205a1
--global <31090859+mike-fam@users.noreply.github.com>
2024-12-03 09:55:00 +1000

    Update code with nicer solution

--- src/day_2/main.py
+++ src/day_2/main.py
@@ -5,7 +5,8 @@ def part1():
     safe_count = 0
     with open("input.txt") as fin:
         for line in fin:
-            valid = part1_validate(list(map(int, line.strip().split())))
+            parsed_line = list(map(int, line.strip().split()))
+            valid = part1_validate(parsed_line)
             if valid:
                 safe_count += 1
     print("Part 1:", safe_count)
@@ -37,15 +38,21 @@ def part2():
     safe_count = 0
     with open("input.txt") as fin:
         for line in fin:
+            parsed_line = list(map(int, line.strip().split()))
+
+            # Pick a way to run this
             # valid = part2_validate_bruteforce(list(map(int, line.strip().split())), True)
-            valid = part2_validate_efficient(list(map(int, line.strip().split())), True)
+            # valid = part2_validate_efficient(list(map(int, line.strip().split())), True)
+            valid = (
+                part2_validate_efficient_nicer(parsed_line, True) or
+                part2_validate_efficient_nicer(list(reversed(parsed_line)), True))
             if valid:
                 safe_count += 1
     print("Part 2:", safe_count)
 
 
 # O(n) (O(4n) to be exact), not very elegant
-def part2_validate_efficient(nums: list[int], toleratable=False):
+def part2_validate_efficient(nums: list[int], tolerable=False):
     previous = None
     ascending = None
     for index, current in enumerate(nums):
@@ -54,7 +61,7 @@ def part2_validate_efficient(nums: list[int], toleratable=False):
             continue
 
         if not (1 <= abs(current - previous) <= 3):
-            if not toleratable:
+            if not tolerable:
                 return False
             # Assume that problematic number is either at index, index - 1, or index - 2
             return (
@@ -69,7 +76,7 @@ def part2_validate_efficient(nums: list[int], toleratable=False):
             continue
         
         if (current > previous) is not ascending:
-            if not toleratable:
+            if not tolerable:
                 return False
             # Assume that problematic number is either at index, index - 1, or index - 2
             return (
@@ -77,6 +84,34 @@ def part2_validate_efficient(nums: list[int], toleratable=False):
                 part2_validate_efficient(nums[:index - 1] + nums[index:]) or
                 part2_validate_efficient(nums[:index - 2] + nums[index - 1:])
             )
+
+        previous = current
+    return True
+
+
+# O(n) (O(4n) to be exact), a bit more elegant
+def part2_validate_efficient_nicer(nums: list[int], tolerable=False):
+    previous = None
+    ascending = None
+    for index, current in enumerate(nums):
+        if previous is None:
+            previous = current
+            continue
+
+        if not (1 <= abs(current - previous) <= 3):
+            if not tolerable:
+                return False
+            return part2_validate_efficient_nicer(nums[:index] + nums[index + 1:])
+            
+        if ascending is None:
+            ascending = current > previous
+            previous = current
+            continue
+        
+        if (current > previous) is not ascending:
+            if not tolerable:
+                return False
+            return part2_validate_efficient_nicer(nums[:index] + nums[index + 1:])
 
 
         previous = current
@@ -113,6 +148,18 @@ def part2_validate_bruteforce(nums, tolerable=False):
 
         previous = current
     return True
+
+
+def part2_validate_raf(nums):
+    if len(nums) < 2:
+        return 0
+    unsafe = 0
+    increasing = nums[1] - nums[0] > 0
+    for i in range(len(nums) - 1):
+        delta = nums[i + 1] - nums[i] * (1 if increasing else -1)
+        if delta < 1 or delta > 3:
+            unsafe += 1
+    return unsafe <= 1
 
 
 if __name__ == "__main__":
