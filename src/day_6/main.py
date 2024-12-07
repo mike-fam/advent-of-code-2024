619557db6c023360980c8ae8e798e68e34e1dce2
--global <31090859+mike-fam@users.noreply.github.com>
2024-12-07 20:23:33 +1000

    Day 6 - Part 2

--- src/day_6/main.py
+++ src/day_6/main.py
@@ -1,6 +1,7 @@
+import time
 from collections import defaultdict
-from itertools import chain
-from pprint import pprint
+from functools import wraps
+from itertools import product
 
 # Left, up, right, down, clockwise
 DIRECTIONS = [
@@ -9,6 +10,19 @@ DIRECTIONS = [
     (1, 0),
     (0, 1),
 ]
+
+def timed(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {elapsed_time:.4f} seconds.")
+        return result
+
+    return wrapper
+
 
 def load_data():
     obstacles = set()
@@ -28,6 +42,7 @@ def move(position, direction):
     return position[0] + direction[0], position[1] + direction[1]
 
 
+@timed
 def part1():
     (size_x, size_y), position, obstacles = load_data()
     positions_passed = set()
@@ -50,51 +65,40 @@ def part1():
     print("Part 1:", len(positions_passed))
 
 
-
+@timed
 def part2():
     (size_x, size_y), start_position, obstacles = load_data()
-    direction_index = 1  # Up
-    new_obstacles = defaultdict(lambda: defaultdict(set))
-    hit_obstacles = defaultdict(set)
-    position = start_position
-    while True:
-        pos_x, pos_y = position
-        if not (0 <= pos_x < size_x):
-            break
-        if not (0 <= pos_y < size_y):
-            break
-        
-        # Scan for obstacles on the right
-        direction = DIRECTIONS[direction_index]
-        right_direction = DIRECTIONS[(direction_index + 1) % len(DIRECTIONS)]
-        scan_pos = position
+    new_obstacles = set()
+    potential_new_obstacles = product(range(size_x), range(size_y))
+    for potential_obstacle in potential_new_obstacles:
+        if potential_obstacle in obstacles or potential_obstacle == start_position:
+            continue
+        direction_index = 1  # Up
+        position = start_position
+        test_obstacles = obstacles.copy()
+        test_obstacles.add(potential_obstacle)
+        passed_positions = defaultdict(set)
         while True:
-            scan_pos = move(scan_pos, right_direction)
-            scan_pos_x, scan_pos_y = scan_pos
-            if not (0 <= scan_pos_x < size_x):
+            pos_x, pos_y = position
+            if not (0 <= pos_x < size_x):
                 break
-            if not (0 <= scan_pos_y < size_y):
+            if not (0 <= pos_y < size_y):
                 break
-            next_pos = move(position, direction)
-            if scan_pos in obstacles and next_pos not in obstacles and next_pos != start_position:
-                new_obstacles[right_direction][scan_pos].add(next_pos)
+            if direction_index in passed_positions[position]:
+                # Now in a loop
+                new_obstacles.add(potential_obstacle)
                 break
-                
-        # Try to move, if not possible, then keep turning
-        while True:
-            direction = DIRECTIONS[direction_index]
-            potential_move = move(position, direction)
-            if potential_move in obstacles:
-                direction_index = (direction_index + 1) % len(DIRECTIONS)
-                hit_obstacles[direction].add(potential_move)
-            else:
-                position = potential_move
-                break
-    new_obstacle_positions = []
-    for new_obstacles_at_direction in new_obstacles.values():
-        for new_obstacle_positions_at_direction in new_obstacles_at_direction.values():
-            new_obstacle_positions.extend(new_obstacle_positions_at_direction)
-    print("Part 2:", len(new_obstacle_positions))
+            passed_positions[position].add(direction_index)
+            # Try to move, if not possible, then keep turning
+            while True:
+                direction = DIRECTIONS[direction_index]
+                potential_move = move(position, direction)
+                if potential_move in test_obstacles:
+                    direction_index = (direction_index + 1) % len(DIRECTIONS)
+                else:
+                    position = potential_move
+                    break
+    print("Part 2:", len(new_obstacles))
 
 
 if __name__ == '__main__':
